a7e3d306fb88fe8c96f5468a048cde954f984037
Diego Albitres <diego.albitres@gmail.com>
2022-05-03 18:46:29 -0500

    chore: add tailwind theme and postcss config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    extend: {
+      colors: {
+        ceil: "#90adc6",
+        space: "#434d59",
+        pewter: "#e9eaec",
+        sunglow: "#fad02c",
+        sunny: "#edef7c",
+      },
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
