ec2e7a8f181653042206705eb065140b5931c1b7
Diego Albitres <diego.albitres@gmail.com>
2022-05-03 18:54:02 -0500

    feat: add tailwind to base.css
    * Also added overrides that might conflict with tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <script setup></script>
 
 <template>
-  <div>Hello, world!</div>
+  <div class="text-lg">Hello, world!</div>
 </template>
-
-<style>
-@import "primevue/resources/themes/mdc-light-indigo/theme.css";
-@import "primevue/resources/primevue.css";
-@import "primeicons/primeicons.css";
-@import "@/assets/base.css";
-</style>
