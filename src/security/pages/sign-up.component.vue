955bdafa15246466fbd0a593ed9139a1e1c1524c
Angel Meneses <angelmenesest123@gmail.com>
2022-05-04 13:56:13 -0500

    feat(auth): implemented base layout for sign-up

---
+++ src/security/pages/sign-up.component.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col items-center w-100 space-y-4">
+    <span class="text-2xl font-semibold"> Create an account </span>
+    <div class="flex flex-col w-full space-y-4">
+      <input type="text" placeholder="Fullname" class="h-12" />
+      <input type="email" placeholder="Email" class="h-12" />
+      <input type="password" placeholder="Password" class="h-12" />
+      <input type="tel" placeholder="Phone" class="h-12" />
+      <input type="text" placeholder="Role" class="h-12" />
+    </div>
+    <div>
+      <input type="checkbox" />
+      <span clas> Agree with terms and conditions</span>
+    </div>
+    <button class="">Sign Up</button>
+    <div>
+      <span>Already on WAW? </span>
+      <span class="font-semibold"> Sign in </span>
+    </div>
+  </div>
+</template>
