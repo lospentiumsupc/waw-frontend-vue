c484f06403971ab26306d97b08c719cd2105f1d6
Diego Albitres <diego.albitres@gmail.com>
2022-05-03 20:36:02 -0500

    fix: added bold property to logo in footer

--- src/components/base-footer.vue
+++ src/components/base-footer.vue
@@ -12,7 +12,7 @@ const selectedLang = ref(languages[0]);
 <template>
   <footer class="py-8 px-16 flex justify-between bg-slate-50">
     <div class="space-y-4 flex flex-col">
-      <span class="mb-4 text-2xl">WAW</span>
+      <span class="mb-4 text-2xl font-bold">WAW</span>
       <span class="text-sm font-light">© Future Leaders Inc., 2022</span>
     </div>
     <nav class="space-x-24 flex text-xs text-slate-800">
@@ -36,6 +36,7 @@ const selectedLang = ref(languages[0]);
       <span class="mb-4 text-sm font-light">Language</span>
       <Dropdown
         v-model="selectedLang"
+        class="text-xs"
         :options="languages"
         option-label="label"
         :placeholder="selectedLang.label" />
