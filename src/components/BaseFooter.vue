eb7af3baeff4eb81e96dc99e0f3b5b5e53e4381f
Diego Albitres <diego.albitres@gmail.com>
2022-05-03 19:43:57 -0500

    feat: added footer component

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Dropdown from "primevue/dropdown";
+import { ref } from "vue";
+
+const languages = [
+  { label: "English", code: "en_US" },
+  { label: "Spanish", code: "es_PE" },
+];
+const selectedLang = ref(languages[0]);
+</script>
+
+<template>
+  <footer class="py-8 px-16 flex justify-between bg-slate-50">
+    <div class="space-y-4 flex flex-col">
+      <span class="mb-4 text-2xl">WAW</span>
+      <span class="text-sm font-light">© Future Leaders Inc., 2022</span>
+    </div>
+    <nav class="space-x-24 flex text-xs text-slate-800">
+      <div class="space-y-4 flex flex-col">
+        <span>About</span>
+        <span>Careers</span>
+        <span>Advertising</span>
+        <span>Small Business</span>
+      </div>
+      <div class="space-y-4 flex flex-col">
+        <span>Safety Center</span>
+        <span>Knowledge Base</span>
+        <span>Contact Us</span>
+      </div>
+      <div class="space-y-4 flex flex-col">
+        <span>Community Guidelines</span>
+        <span>Privacy & Terms</span>
+      </div>
+    </nav>
+    <div class="flex flex-col">
+      <span class="mb-4 text-sm font-light">Language</span>
+      <Dropdown
+        v-model="selectedLang"
+        :options="languages"
+        option-label="label"
+        :placeholder="selectedLang.label" />
+    </div>
+  </footer>
+</template>
