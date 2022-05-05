fdac44e47576805b160ad7e43d1c747f37f51ca6
Angel Meneses <angelmenesest123@gmail.com>
2022-05-04 21:24:22 -0500

    feat(auth): defined styles in sign-up component

--- src/security/pages/sign-up.component.vue
+++ src/security/pages/sign-up.component.vue
@@ -1,23 +1,44 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col items-center w-100 space-y-4">
-    <span class="text-2xl font-semibold"> Create an account </span>
-    <div class="flex flex-col w-full space-y-4">
-      <input type="text" placeholder="Fullname" class="h-12" />
-      <input type="email" placeholder="Email" class="h-12" />
-      <input type="password" placeholder="Password" class="h-12" />
-      <input type="tel" placeholder="Phone" class="h-12" />
-      <input type="text" placeholder="Role" class="h-12" />
+  <div class="flex flex-col items-center mt-8">
+    <div class="py-8">
+      <span class="text-2xl font-semibold"> Create an account </span>
     </div>
-    <div>
+    <input
+      type="text"
+      class="bg-slate-400 w-100 rounded-lg h-12 placeholder:font-light placeholder:text-white my-4"
+      placeholder="Fullname" />
+    <input
+      type="email"
+      class="bg-slate-400 rounded-lg h-12 placeholder:font-light placeholder:text-white my-4"
+      placeholder="Email" />
+    <input
+      type="password"
+      class="bg-slate-400 rounded-lg h-12 placeholder:font-light placeholder:text-white my-4"
+      placeholder="Password" />
+    <input
+      type="tel"
+      class="bg-slate-400 rounded-lg h-12 placeholder:font-light placeholder:text-white my-4"
+      placeholder="Phone" />
+    <input
+      type="password"
+      class="bg-slate-400 rounded-lg h-12 placeholder:font-light placeholder:text-white my-4"
+      placeholder="Rol" />
+
+    <div class="py-4">
       <input type="checkbox" />
       <span clas> Agree with terms and conditions</span>
     </div>
-    <button class="">Sign Up</button>
     <div>
+      <button
+        class="bg-slate-500 border-0 fs-bold hover:bg-slate-700 text-white fs-semibold rounded-xl w-40 h-12">
+        Sign Up
+      </button>
+    </div>
+    <div class="py-1">
       <span>Already on WAW? </span>
-      <span class="font-semibold"> Sign in </span>
+      <button type="submit" class="font-semibold">Sign in</button>
     </div>
   </div>
 </template>
