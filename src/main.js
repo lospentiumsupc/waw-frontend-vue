c0e95ab5b5f2661e1afe3ccbdd9ffa72ccde0385
Angel Meneses <angelmenesest123@gmail.com>
2022-05-27 01:41:50 -0500

    feat(jobs): added confirmation and toast services

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import { Router } from "./router";
 import PrimeVue from "primevue/config";
 import { Vue3Mq } from "vue3-mq";
+import ConfirmationService from "primevue/confirmationservice";
+import ToastService from "primevue/toastservice";
 import "@/assets/base.css";
 
 const app = createApp(App);
@@ -10,5 +12,7 @@ const app = createApp(App);
 app.use(Router);
 app.use(PrimeVue);
 app.use(Vue3Mq, { preset: "tailwind" });
+app.use(ConfirmationService);
+app.use(ToastService);
 
 app.mount("#app");
